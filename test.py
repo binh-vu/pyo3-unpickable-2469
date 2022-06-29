f69cb2789b91b7f6d8dfa17af3196867baf78d80
Binh Vu <binh@toan2.com>
2022-06-28 23:06:14 -0700

    update

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import pickle
 from demo import square, sub1
-from demo.sub1 import cube
 
 print(square(5))
 print(square(10))
 print(sub1.cube(10))
 
+# error
 pickle.dumps(sub1.cube)
 
+# also error
 cube = sub1.cube
 pickle.dumps(cube)
+
+# cannot import without using sys.modules trick
+from demo.sub1 import cube
